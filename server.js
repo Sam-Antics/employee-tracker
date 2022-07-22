c9f8a75d97fd918787769bde28bc8b43b73d69c3
Samantha Guerra <88997930+Sam-Antics@users.noreply.github.com>
2022-07-22 03:15:19 -0500

    Express server connected and tested

---
+++ server.js
@@ -0,0 +1,27 @@
+// importing Express.js
+const express = require('express');
+
+// PORT designation and app expression
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Yo, bitch'
+  });
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+// starts Express.js server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
